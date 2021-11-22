0c5da20b07acb44d96ca6e9e24db72314ae66d4b
gabrieldvpereira <gdavi.gd@gmail.com>
2021-11-21 21:04:33 -0300

    feat: create auth repository

---
+++ authentication_repository/lib/src/models/user.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  final String? email;
+  final String id;
+  final String? name;
+  final String? photo;
+
+  const User({
+    required this.id,
+    this.email,
+    this.name,
+    this.photo,
+  });
+
+  factory User.initial() {
+    return User(id: '');
+  }
+
+  bool get isEmpty => this == User.initial();
+  bool get isNotEmpty => this != User.initial();
+
+  @override
+  List<Object?> get props => [id, email, name, photo];
+}
